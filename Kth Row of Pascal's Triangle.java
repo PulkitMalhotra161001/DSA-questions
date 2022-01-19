3f02b257d8bc0f7257598d3069107ad8dcc69aba
Pulkit Malhotra <63710339+PulkitMalhotra161001@users.noreply.github.com>
2022-01-20 00:12:46 +0530

    Update Kth Row of Pascal's Triangle.java

--- Kth Row of Pascal's Triangle.java
+++ Kth Row of Pascal's Triangle.java
@@ -12,3 +12,20 @@ public class Solution {
 	    return res;
     }
 }
+
+// ArrayList<Long> nthRowOfPascalTriangle(int n) {
+// 	if(n==1){
+// 	    ArrayList<Long> al=new ArrayList<>();
+// 	    al.add(1L);
+// 	    return al;
+// 	}
+//         ArrayList<Long> prev_list=nthRowOfPascalTriangle(n-1),curr_list=new ArrayList<>();
+			
+// 	for(int j=0;j<n;j++) {
+//             if(j==0 || j==n-1)
+//                 curr_list.add(1L);
+//             else
+// 		curr_list.add((prev_list.get(j)+prev_list.get(j-1))%1000000007);
+// 	}
+//         return curr_list;
+// }
