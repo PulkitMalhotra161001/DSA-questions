3655ae3019823b42e151ffa5165d78e780bdf457
Pulkit Malhotra <63710339+PulkitMalhotra161001@users.noreply.github.com>
2022-01-30 19:53:40 +0530

    Update int to binary to int.java

--- int to binary to int.java
+++ int to binary to int.java
@@ -3,3 +3,8 @@ String s=Integer.toBinaryString(x); //int to binary
 System.out.println(s);
 x=Integer.parseInt(s, 2); //binary to int
 System.out.println(x);
+
+
+//binary search
+int id = Collections.binarySearch(a,b);
+return id<0?-id-1:id;
