363f06d4336ac9464241fa7b8247b71ff8ebf546
Pulkit Malhotra <63710339+PulkitMalhotra161001@users.noreply.github.com>
2022-02-09 08:33:14 +0530

    Update Sort by Set Bit Count - comparator - lambda func.java

--- Sort by Set Bit Count - comparator - lambda func.java
+++ Sort by Set Bit Count - comparator - lambda func.java
@@ -3,7 +3,7 @@ class Compute
     static void sortBySetBitCount(Integer arr[], int n)
     { 
         //lambda function
-        //comparator function comapre
+        //comparator function compare
         //decending order
         Arrays.sort(arr, (x,y)->Integer.compare(Integer.bitCount(y), Integer.bitCount(x)));
     }
