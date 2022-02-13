22f3609d54c05f4b7f6c2da1039b82e16731f262
Pulkit Malhotra <63710339+PulkitMalhotra161001@users.noreply.github.com>
2022-02-13 22:19:29 +0530

    Create min coins.java

---
+++ min coins.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int c = 0;
+        int max = 0;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp,amount+1);
+        dp[0] = 0;
+        for (int coin: coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+        return dp[amount]>amount?-1:dp[amount];
+    }
+}
