a2e19c730535301ed7fd1323874a15686bd15acc
Pulkit Malhotra <63710339+PulkitMalhotra161001@users.noreply.github.com>
2022-02-18 00:15:35 +0530

    Update Corona Vaccine.java

--- Corona Vaccine.java
+++ Corona Vaccine.java
@@ -2,16 +2,16 @@ class Solution{
     static int count;
     public static int supplyVaccine(Node root){
         count=0;
-        if(solution(root)==-1)
+        if(postOrder(root)==-1)
             return count+1;
         return count;
     }
-    public static int solution(Node root){
+    public static int postOrder(Node root){
         if(root==null){
             return 1;
         }
-        int left=solution(root.left);
-        int right=solution(root.right);
+        int left=postOrder(root.left);
+        int right=postOrder(root.right);
         if(left==-1 || right==-1){
             count++;
             return 0;
