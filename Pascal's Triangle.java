726ea09a6faa02c5ad0e5dc5dc791209ef0adbca
Pulkit Malhotra <63710339+PulkitMalhotra161001@users.noreply.github.com>
2022-02-19 23:20:52 +0530

    Update Pascal's Triangle.java

--- Pascal's Triangle.java
+++ Pascal's Triangle.java
@@ -1,19 +1,19 @@
 class Solution {
-    public List<List<Integer>> generate(int n) {
-        List<List<Integer>> l=new ArrayList<>();
-        List<Integer> prev_list=null,curr_list;
-			
-		for(int i=0;i<n;i++) {
-            curr_list=new ArrayList<>();
-			for(int j=0;j<=i;j++) {
-                if(j==0 || j==i)
-                    curr_list.add(1);
-                else
-    				curr_list.add(prev_list.get(j)+prev_list.get(j-1));
+	public List<List<Integer>> generate(int n) {
+		List<List<Integer>> l = new ArrayList<>();
+		List<Integer> prev_list = null, curr_list;
+
+		for (int i = 0; i < n; i++) {
+			curr_list = new ArrayList<>();
+			for (int j = 0; j <= i; j++) {
+				if (j == 0 || j == i)
+					curr_list.add(1);
+				else
+					curr_list.add(prev_list.get(j) + prev_list.get(j - 1));
 			}
-            prev_list=curr_list;
+			prev_list = curr_list;
 			l.add(curr_list);
 		}
-        return l;
-    }
+		return l;
+	}
 }
