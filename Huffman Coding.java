51ca6fc6c1d71a688dac6618f3087b5ded251e11
Pulkit Malhotra <63710339+PulkitMalhotra161001@users.noreply.github.com>
2022-04-27 00:59:43 +0530

    Create Huffman Coding.java

---
+++ Huffman Coding.java
@@ -0,0 +1,42 @@
+class Solution {
+    class Huffman{
+        char c;
+        int val;
+        Huffman left;
+        Huffman right;
+        public Huffman(char c,int val){
+            this.c=c;
+            this.val=val;
+        }
+    }
+    public ArrayList<String> huffmanCodes(String S, int f[], int N)
+    {
+        PriorityQueue<Huffman> min_heap=new PriorityQueue<>((o1,o2)->o1.val>=o2.val?1:-1);
+        for(int i=0;i<N;i++){
+            Huffman hm=new Huffman(S.charAt(i),f[i]);
+            min_heap.add(hm);
+        }
+        Huffman root=null;
+        while(min_heap.size()>1){
+            Huffman x=min_heap.poll();
+            Huffman y=min_heap.poll();
+            Huffman temp=new Huffman('-',x.val+y.val);
+            temp.left=x;
+            temp.right=y;
+            root=temp;
+            min_heap.add(temp);
+        }
+        ArrayList<String> ans=new ArrayList<>();
+        preorder(root,ans,"");
+        return ans;
+    }
+    public void preorder(Huffman root,ArrayList<String> ans,String till_now){
+        if(root==null){
+            return;
+        }
+        if(Character.isLetter(root.c))
+            ans.add(till_now);
+        preorder(root.left,ans,till_now+"0");
+        preorder(root.right,ans,till_now+"1");
+    }
+}
