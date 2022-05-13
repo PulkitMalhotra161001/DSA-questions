7e030ea1f0ca815a071277e341b27243d78e4cbc
Pulkit Malhotra <63710339+PulkitMalhotra161001@users.noreply.github.com>
2022-05-13 18:59:42 +0530

    Update and rename Right View of BT.java to Left and Right View of BT.java

--- Tree/Left and Right View of BT.java
+++ Tree/Left and Right View of BT.java
@@ -6,9 +6,12 @@ class Solution {
     }
     public void dfs(TreeNode root,int depth){
         if(root==null)  return;
+        //if this value is not having anything (assume size=0 && depth=0)
         if(ans.size()==depth){
             ans.add(root.val);
         }
+        //traverse right (first) for right view
+        //or traverse left (first) for left view
         dfs(root.right,depth+1);
         dfs(root.left,depth+1);
     }
