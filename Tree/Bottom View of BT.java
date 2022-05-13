04559739cf9ddcb4a080fdd449fbe9620ef3d4ff
Pulkit Malhotra <63710339+PulkitMalhotra161001@users.noreply.github.com>
2022-05-13 19:35:08 +0530

    Create Bottom View of BT.java

---
+++ Tree/Bottom View of BT.java
@@ -0,0 +1,47 @@
+class Solution
+{
+    //for storing current node and dist (horizontal)
+    class Details{
+        Node root;
+        int dist;
+        public Details(Node r,int d){
+            root=r;
+            dist=d;
+        }
+    }
+    //Function to return a list containing the bottom view of the given tree.
+    public ArrayList <Integer> bottomView(Node root)
+    {
+        // for level order traversal
+        Queue<Details> q=new LinkedList<>();
+        // for storing the root.val at horizontal distance
+        Map<Integer,Integer> map=new TreeMap<>();
+        if(root!=null)
+            q.add(new Details(root,0));
+            
+        while(!q.isEmpty()){
+            Details pop=q.remove();
+            
+            Node node=pop.root;
+            int dist=pop.dist;
+            // we put every element irrespective or the case
+            // put the parent ele
+            // if child have some value then they will change as this is bottom view
+            map.put(dist,node.data);
+            
+            if(node.left!=null){
+                // for left horizonal distance is decreases
+                q.add(new Details(node.left,dist-1));
+            }
+            if(node.right!=null){
+                // for right horizonal distance is increases
+                q.add(new Details(node.right,dist+1));
+            }
+        }
+        ArrayList<Integer> al=new ArrayList<>();
+        for(Integer k:map.keySet()){
+            al.add(map.get(k));
+        }
+        return al;
+    }
+}
