bfc7804dd9a15061475aa719884cc7763b22ada5
Pulkit Malhotra <63710339+PulkitMalhotra161001@users.noreply.github.com>
2022-07-01 22:13:26 +0530

    Create sort 2d array.java

---
+++ List/sort 2d array.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int arr[][]={{1,7},{9,1},{6,3},{7,2},{1,2}};
+        print(arr);
+        Arrays.sort(arr,(o1,o2)->o1[1]-o2[1]);
+        print(arr);
+    }
+    public static void print(int arr[][]){
+        for(int i[]:arr){
+            for(int j:i){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
