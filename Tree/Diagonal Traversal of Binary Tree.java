66218937fc1f571e12c497c68426f858c1088da6
Pulkit Malhotra <63710339+PulkitMalhotra161001@users.noreply.github.com>
2022-07-06 16:54:04 +0530

    Create Diagonal Traversal of Binary Tree.java

---
+++ Tree/Diagonal Traversal of Binary Tree.java
@@ -0,0 +1,30 @@
+class Tree
+{
+    public ArrayList<Integer> diagonal(Node root)
+    {
+        
+        TreeMap<Integer,ArrayList<Integer>> map=new TreeMap<>();
+        
+        fill(map,root,0);
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        for(Integer key:map.keySet()){
+            ans.addAll(map.get(key));
+        }
+        
+        return ans;
+        
+    }
+    public void fill(TreeMap<Integer,ArrayList<Integer>> map,Node root,int level){
+        if(root==null)  return;
+        ArrayList<Integer> temp = new ArrayList<>();
+        if(map.containsKey(level)){
+            temp=map.get(level);
+        }
+        temp.add(root.data);
+        map.put(level,temp);
+        fill(map,root.left,level+1);
+        fill(map,root.right,level);
+    }
+}
