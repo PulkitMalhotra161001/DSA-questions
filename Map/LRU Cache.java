a99376ebb655561cace44936d5abcb0b7f83bf9b
Pulkit Malhotra <63710339+PulkitMalhotra161001@users.noreply.github.com>
2022-11-15 15:49:21 +0530

    Update LRU Cache.java

--- Map/LRU Cache.java
+++ Map/LRU Cache.java
@@ -99,3 +99,80 @@ class LRUCache {
         head.next=node;
     }
 }
+
+
+_______________________________________________________________________________________________________________________________________________________________
+//approach 2 : LinkedHashMap
+
+class LRUCache {
+    Map<Integer,Integer> map;
+    int n;
+    public LRUCache(int capacity) {
+        map=new LinkedHashMap<>();
+        n=capacity;
+    }
+    
+    public int get(int key) {
+        int ans = map.getOrDefault(key,-1);
+        //if it contains
+        if(ans>-1){
+            //we removed the element
+            map.remove(key);
+            //and pushed back
+            map.put(key,ans);
+        }
+        return ans;
+    }
+    
+    public void put(int key, int value) {
+        
+        //if it reached size and doesn't contain key and remove least used key
+        if(map.size()>=n && !map.containsKey(key))    map.remove( map.keySet().iterator().next() );
+        
+        //if map contain key then just remove it and pushed back
+        if(map.containsKey(key))   map.remove(key);
+        
+        //if not contain then added
+        map.put(key,value);
+    }
+}
+
+
+_______________________________________________________________________________________________________________________________________________________________
+//way 2
+
+class LRUCache extends LinkedHashMap<Integer,Integer>{
+    
+    int n;
+    
+    public LRUCache(int capacity) {
+        
+        //size => capacity
+        //load factor => 0.75f
+        //accessOrder  => true means ordering mode
+		//                false means insertion mode
+        super(capacity,0.75f,true);
+        n=capacity;
+    }
+    
+    public int get(int key) {
+        return super.getOrDefault(key,-1);
+    }
+    
+    public void put(int key, int value) {
+        super.put(key,value);
+    }
+    
+/*      removeEldestEntry is useful if the map represents a cache: it allows
+	    the map to reduce memory consumption by deleting stale entries.	It 
+		work if the accessOrder is true
+
+		eldest The least recently inserted entry in the map, or if
+	    this is an access-ordered map, the least recently accessed
+	    entry.
+*/
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer,Integer> eldest){
+        return super.size()>n;
+    }
+}
