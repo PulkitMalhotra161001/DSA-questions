d5225da1ea9b9e87ba747beb91065abd6a678ee0
Pulkit Malhotra <63710339+PulkitMalhotra161001@users.noreply.github.com>
2024-08-09 23:15:00 +0530

    Update Egg Dropping Puzzle.java

--- DP/Egg Dropping Puzzle.java
+++ DP/Egg Dropping Puzzle.java
@@ -16,29 +16,31 @@ static int eggDrop(int n, int k)
     for(int i=2;i<=n;i++){
         for(int j=2;j<=k;j++){
 
-            //maximum will be the no. of floors
-            int max=j;
-
-            //check if egg break or survive at this level
-            //we are checking it for every time because floor and egg change
+            //we have to take the best case in the worst_case scenarios (best of worst) (min of max)
+            int min_attempt = Integer.MAX_VALUE;
+            
+            //drop the egg from every floor
+            //take max from those (egg_break and egg_survive)
+            //from all the possibilities take min (this ensure that our work is done)
             for(int t=1;t<=j;t++){
 
                 //egg break so no of eggs left egg-1 and floor left this floor upto ground
-                int egg_break=   1+dp[i-1][t-1];
+                int egg_break = 1+dp[i-1][t-1];
 
                 //egg survived means break point not reached
-                int egg_survived=1+dp[i][j-t];
+                int egg_survived = 1+dp[i][j-t];
 
-                //take max (employee worst time vs best time)
-                //we assume luck is not in our favour
-                int ans=Math.max(egg_break,egg_survived);
+                //eggs broke at the 'kth floor' (egg-1|k-1) or not (egg|total_floor-k)
+                // take the worst case scenarios
+                int current_attempt=Math.max(egg_break,egg_survived);
 
                 //take min from worst cases (best time we are sure of)
-                max=Math.min(max,ans);
+                min_attempt=Math.min(min_attempt,current_attempt);
             }
 
             //fill result in dp (for this much of floors & this much eggs)
-            dp[i][j]=max;
+            //we have to take the best case in the worst_case scenarios (best of worst) (min of max)
+            dp[i][j]=min_attempt;
         }
     }
     return dp[n][k];
